7e196555723b0d3ed544b468e00596b8db7c257e
amitkbiswas01 <amitkbiswas01@gmail.com>
2023-06-04 16:41:49 +0600

    update: todo-list with todo-item, styles

--- src/app/components/todo-list/todo-list.module.ts
+++ src/app/components/todo-list/todo-list.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { TodoListComponent } from './todo-list.component';
 import { TodoFormModule } from '../todo-form/todo-form.module';
+import { TodoItemModule } from '../todo-item/todo-item.module';
 
 @NgModule({
   declarations: [TodoListComponent],
-  imports: [CommonModule, TodoFormModule],
+  imports: [CommonModule, TodoFormModule, TodoItemModule],
   exports: [TodoListComponent],
 })
 export class TodoListModule {}
